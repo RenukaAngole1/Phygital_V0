3a4032df1568ba3efb1d39eb3485d03ad4d29e1a
RenukaAngole1 <53440430+RenukaAngole1@users.noreply.github.com>
2020-10-09 13:11:02 +0530

    Add files via upload

--- Phygital_V0/setup.py
+++ Phygital_V0/setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'ME',                   # Type in your name
   author_email = 'renuka.angole@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/RenukaAngole1/Phygital_V0',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/RenukaAngole1/Phygital_V0/archive/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/RenukaAngole1/Phygital_V0/archive/0.2.tar.gz',    # I explain this later on
   keywords = ['Robotics', 'Sensors', 'Motors'],   # Keywords that define your package best
   install_requires=[ 'pyserial'           # I get to this in a second
           
